23cdb222e81b2ae3034d8d216dafbfdf575a2a48
Hadryan Silva <hadryan2001@gmail.com>
2025-02-20 18:24:24 -0300

    Initial Commit

---
+++ src/main/java/br/com/retail/smart/RetailProductApplication.java
@@ -0,0 +1,13 @@
+package br.com.retail.smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/retail/smart/RetailProductApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.retail.smart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
